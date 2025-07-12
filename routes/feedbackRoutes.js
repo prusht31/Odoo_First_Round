7904826581458f920718cb08b46a55b04a904883
prusht31 <23dce008@charusat.edu.in>
2025-07-12 14:51:41 +0530

    Delete routes directory

--- routes/feedbackRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const protect = require('../middleware/auth');
-const { leaveFeedback, getFeedbackForUser } = require('../controllers/feedbackController');
-
-router.post('/', protect, leaveFeedback);
-router.get('/:userId', getFeedbackForUser);
-
-module.exports = router;
