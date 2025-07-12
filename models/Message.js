f8ec31a7c148f0093f134cb620a3584363aa121b
prusht31 <23dce008@charusat.edu.in>
2025-07-12 14:51:24 +0530

    Delete models directory

--- models/Message.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const MessageSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-}, { timestamps: true });
-
-module.exports = mongoose.model('Message', MessageSchema);
